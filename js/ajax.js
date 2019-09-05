42046ff493b81965ff143b219a2fb0b66e58b3d4
Jim <jim@advancebass.com>
2019-09-04 23:13:45 -0700

    format with prettier

--- js/ajax.js
+++ js/ajax.js
@@ -1,7 +1,9 @@
 // preload loader gif
-$('body').append('<img id="preload" style="display:none;" src="img/loader.gif">');
+$("body").append(
+  '<img id="preload" style="display:none;" src="img/loader.gif">'
+);
 
-$('#ajax-btn').on('click', function (event) {
+$("#ajax-btn").on("click", function(event) {
   event.preventDefault();
   loadImages();
 });
@@ -10,8 +12,7 @@ $('#ajax-btn').on('click', function (event) {
  * Load images
  */
 function loadImages() {
-
-  $('#ajax-content').empty();
+  $("#ajax-content").empty();
 
   var numImages = $("#num-images").val();
   numImages = parseInt(numImages);
@@ -22,27 +23,27 @@ function loadImages() {
   var imgWidth = $("#img-width").val();
   var imgHeight = $("#img-height").val();
 
-  $('#ajax-content').append('<img class="preloader" src="img/loader.gif">');
+  $("#ajax-content").append('<img class="preloader" src="img/loader.gif">');
 
-  var url = 'https://picsum.photos/' + imgWidth + '/' + imgHeight + '?image=';
+  var url = "https://picsum.photos/" + imgWidth + "/" + imgHeight + "?image=";
 
   // loop and append images
   while (imgIndex < imgOffset) {
-    var img = '';
-    img += '<a class="post-link" href="' + url + imgIndex + '" target="_blank">';
+    var img = "";
+    img +=
+      '<a class="post-link" href="' + url + imgIndex + '" target="_blank">';
     img += '<img class="post-img" src="' + url + imgIndex + '"/>';
-    img += '</a>';
+    img += "</a>";
 
-    $('#ajax-content').append(img);
+    $("#ajax-content").append(img);
     imgIndex++;
   }
 
   // if img src is not found, sorry console errors :(
-  $('img').on('error', function () {
-    $(this).css('display', 'none');
+  $("img").on("error", function() {
+    $(this).css("display", "none");
   });
 
   // remove loader gif
-  $('.preloader').remove();
-
+  $(".preloader").remove();
 }
